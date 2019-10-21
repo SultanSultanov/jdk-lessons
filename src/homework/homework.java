711891d4e43ed8eeb2df0b58b650519238fc29de
Sultanov <sull2007@list.ru>
2019-10-21 18:50:14 +0300

    Initial commit

---
+++ src/homework/homework.java
@@ -0,0 +1,78 @@
+package homework;
+
+import java.util.Scanner;
+
+public class homework {
+
+
+    public static void main(String[] args) {
+        homework1();
+        homework2();
+        homework3();
+    }
+
+    private static void homework1() {
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int a = s.nextInt();
+        System.out.print("Введите второе число: ");
+        int b = s.nextInt();
+
+        for (; ; ) {
+            if (a > b) {
+                a = a % b;
+            } else {
+                b = b % a;
+            }
+            if (a == 0) {
+                System.out.println("НОД = " + b);
+                break;
+            }
+            if (b == 0) {
+                System.out.println("НОД = " + a);
+                break;
+            }
+        }
+
+    }
+
+    private static void homework2() {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int d = s.nextInt();
+        StringBuilder str = new StringBuilder();
+
+        int x = 1;
+        for (int i = 0; i < d; i++) {
+            str.append((d & x) == 0 ? "0" : "1");
+            x <<= 1;
+        }
+
+        System.out.println("Результат = " + str.reverse());
+
+    }
+
+    private static void homework3() {
+        int arr[] = {1, 3, 5, 4, 9};
+
+        for (int i = 0; i < arr.length; i++) {
+           for (int x = 0; x < arr.length; x++){
+
+               if (arr[i]+arr[x]==7){
+                   System.out.printf("%d %d", arr[i],arr[x]);
+               }
+           }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
