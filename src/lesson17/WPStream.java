5ea7bcea530d7b37c6e2693cbf478f44528ff1a1
Sultanov <sull2007@list.ru>
2019-11-27 21:56:32 +0300

    test

--- src/lesson17/WPStream.java
+++ src/lesson17/WPStream.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class WPStream {
     public static void main(String[] args) throws IOException {
@@ -12,9 +15,16 @@ public class WPStream {
         File file = new File("resource/wp.txt");
         Map<String, Long> map = Files.lines((file.toPath()))
                 .parallel()
-                .map(String::toLowerCase)
+                .map(line-> line.replaceAll("\\pP}", "").toLowerCase().trim())
                 .flatMap(line -> Arrays.stream(line.split(" ")))
-                .count()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .parallelStream()
+                .sorted((en1, en2)-> en2.getValue().compareTo(en1.getValue()))
+                .limit(10)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        System.out.println(map);
 
     }
 }
